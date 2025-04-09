69bc1d2d5bd9cbc06c2133d62bc9058eac307c89
hnadel123 <hnadel@umich.edu>
2025-04-09 16:58:58 -0400

    adds

--- hannah.py
+++ hannah.py
@@ -1,20 +1,35 @@
-def fetch_weather_data():
-    url = 'http://api.weatherstack.com/current?access_key=114493980920155d0343dfc9594ed16c&query=Anaheim'
-    response = requests.get(url)
-    data = response.json()['current']
+def fetch_weather_data(timestamp):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 34.0522,
+        "longitude": -118.2437,
+        "hourly": "temperature_2m,precipitation,relative_humidity_2m",
+        "timezone": "America/Los_Angeles"
+    }
 
-    temperature = data['temperature']
-    condition = data['weather_descriptions'][0]
-    wind_speed = data['wind_speed'] 
-    humidity = data['humidity']
-    precipitation = data['precip']
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    now = datetime.now().strftime("%Y-%m-%dT%H:00")
+    times = data["hourly"]["time"]
+
+    try:
+        idx = times.index(now)
+    except ValueError:
+        print("Current hour's weather not available.")
+        return
+
+    temperature = data["hourly"]["temperature_2m"][idx]
+    precipitation = data["hourly"]["precipitation"][idx]
+    humidity = data["hourly"]["relative_humidity_2m"][idx]
+    condition = "Rainy" if precipitation > 0 else "Clear"
 
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
 
-    cur.execute('''INSERT INTO Weather (temperature, condition, wind_speed, humidity, precipitation, timestamp)
-                   VALUES (?, ?, ?, ?, ?, ?)''', (temperature, condition, wind_speed, humidity, precipitation, timestamp))
+    cur.execute('''INSERT INTO Weather (temperature, condition, humidity, precipitation, timestamp)
+                   VALUES (?, ?, ?, ?, ?)''', 
+                   (temperature, condition, humidity, precipitation, timestamp))
 
     conn.commit()
     conn.close()
